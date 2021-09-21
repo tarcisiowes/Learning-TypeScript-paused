df2c603916871139d3be5f230d48191374742e5a
Tarcisio Wesley <tarcisiowesley@gmail.com>
2021-09-21 17:23:57 -0300

    typescript types

---
+++ 02-types/index.ts
@@ -0,0 +1,51 @@
+// boolean (true/false)
+let isOpen: boolean
+isOpen = true
+
+//string ('foo', "foo", `foo`)
+let message: string
+message = `foo ${isOpen}`
+
+//number (int, float, hex, binary)
+let Values: number
+Values = 0xff0
+
+// array (type[]) / Array<type>
+let items: string[]
+items = ["foo", "bar"]
+
+let total: Array<number>
+total = [1, 2, 3]
+
+//tuple - two or more
+let title: [number, string, string]
+title = [1, 'foo', 'fii']
+
+//enum
+enum Colors {
+  white = '#fff',
+  black = '#000'
+}
+
+// any
+let coisa: any
+coisa = [1]
+
+// void 
+function logger(): void {
+  console.log('foo')
+}
+
+//null / undefined
+type Wut = string | undefined
+
+//never
+function error(): never{
+  throw new Error('error')
+}
+
+//object
+let cart: object
+cart = {
+  key: '001'
+}
